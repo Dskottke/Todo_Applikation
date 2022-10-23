faecc6fbabb6bd4d4e4eafe15b9ccc38ea2b71e0
davidskottke <davidskottke@googlemail.com>
2022-10-23 18:05:58 +0200

    add styles

--- frontend/src/Components/ToDoCard.tsx
+++ frontend/src/Components/ToDoCard.tsx
@@ -13,9 +13,10 @@ function ToDoCard(props: cardProps) {
         <article>
             <div className={"todocard_id"}>ID: {props.todo.id}</div>
             <div className={"todo_description"}>{props.todo.description}</div>
+
             <section className={"card_buttons"}>
-                {props.todo.status !== "DONE" && <button onClick={() => props.updateToDo(props.todo)}>advance</button>}
-                <button onClick={() => props.deleteToDo(props.todo.id)}>delete</button>
+                {props.todo.status !== "DONE" && <button className={"update_button"} onClick={() => props.updateToDo(props.todo)}>advance</button>}
+                <button className={"delete_button"} onClick={() => props.deleteToDo(props.todo.id)}>delete</button>
             </section>
         </article>
     );
